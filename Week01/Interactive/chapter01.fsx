0e8c8aae6f2ffd47b91bc7128fb1461abb318642
KoHHeKT <1749087+KoHHeKT@users.noreply.github.com>
2026-06-08 20:05:22 +0300

    add generics example

--- Week01/Interactive/chapter01.fsx
+++ Week01/Interactive/chapter01.fsx
@@ -1,2 +1,19 @@
-let getEvenNumbers 
-    = Seq.filter (fun number -> number % 2 = 0)
+let add a b =
+    let answer = a + b + "hello"
+    answer
+
+let explicit = ResizeArray<int>()
+let typeHole = ResizeArray<_>()
+let omitted = ResizeArray()
+
+typeHole.Add 99
+omitted.Add "text"
+
+let combineElements<'T> (a:'T) (b:'T) (c:'T) =
+    let output = ResizeArray<'T>()
+    output.Add a
+    output.Add b
+    output.Add c
+    output
+
+combineElements 1 2 3
